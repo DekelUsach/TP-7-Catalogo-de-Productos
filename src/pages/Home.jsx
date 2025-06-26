309742e7e4054530b3c32e3fe1cd2472550901ff
DEKEL USACH <48386383@ort>
2025-06-26 09:05:43 -0300

    Dimensioned the project and finished navbar, carrousel still in development

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import "../styles/Home.css";
+import Carrousel from '../Components/Carrousel';
 
 export default function Home() {
   return (
     <>
-    
+    <Carrousel></Carrousel>
+        <div>Home</div>
+      
+
 
     
     
--- src/pages/layouts/MainLayout.jsx
+++ src/pages/layouts/MainLayout.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import { CContainer, CNavbar, CNavbarBrand } from "@coreui/react";
-import { NavLink } from "react-router-dom";  
-import {Link, Outlet} from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom";
 import "./MainLayout.css";
 
 export function MainLayout() {
   return (
-    <CNavbar className="bg-body-tertiary">
-      <CContainer fluid>
-        <CNavbarBrand>
-          <NavLink to="/">
-            <img src="/src/assets/logo.png" alt="Home" width="22" height="24" />
-            Home
-          </NavLink>
-          <NavLink to="/productos">
-            Productos
-          </NavLink>
-          <NavLink to="/quienesSomos">
-            Nosotros
-          </NavLink>
-          <NavLink to="/contacto">
-            Contacto
-          </NavLink>
-        </CNavbarBrand>
-      </CContainer>
-      <Outlet/>
-    </CNavbar>
+    <div className="app-layout">
+      <CNavbar className="bg-body-tertiary">
+        <CContainer fluid>
+          <CNavbarBrand>
+            <NavLink to="/" className={"logo link"}>
+              <img src="/src/assets/logo.png" alt="Home" width="22" height="24" />
+              Home
+            </NavLink>
+            <NavLink className={"link"} to="/productos">Productos</NavLink>
+            <NavLink className={"link"} to="/quienesSomos">Nosotros</NavLink>
+            <NavLink className={"link"} to="/contacto">Contacto</NavLink>
+          </CNavbarBrand>
+        </CContainer>
+      </CNavbar>
+
+
+      <main className="app-content">
+        <Outlet />
+      </main>
+
+      
+      <footer className="app-footer">
+        <p>© 2025 Mi Sitio</p>
+      </footer>
+    </div>
   );
 }
