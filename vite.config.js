d3c1d55ffbde9ba96b8d5b414d129faf6b8af856
DekelUsach <48386383@est.ort.edu.ar>
2025-07-13 13:15:33 -0300

    updated vite!

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Detecta si estás en producción
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/TP-7-Catalogo-de-Productos/', // nombre del repo
-})
+  base: isProd ? '/TP-7-Catalogo-de-Productos/' : '/', // ✅ dinámico
+});
