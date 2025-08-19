c168c3fb9e5f9a478a8c567b79c82e101c548067
Android Studio <devandroid@ort>
2025-08-19 09:38:18 -0300

    Added base

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = '/TP-7-Catalogo-de-Productos/'
 
 export default defineConfig({
   plugins: [react()],
-
+  base: repoName,
 })
